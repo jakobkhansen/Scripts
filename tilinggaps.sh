6e99c8ef3cac559aa835958a71c562b098a4dbec
jakobkhansen <jakob.hansen@hotmail.no>
2019-06-28 05:25:12 +0200

    Dolphin stupid

--- tilinggaps.sh
+++ tilinggaps.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 focused_window=$(xdotool getactivewindow)
 
-
 wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
 
 location=$1
@@ -9,25 +8,26 @@ windowX=47.5
 windowY=90
 tilingright=$(awk "BEGIN{print 100 - $windowX}")
 windowname=$(echo $(xdotool getwindowname $focused_window) | awk '{print $NF}')
+echo $windowname
 
-if [ $location == "left" ]
+if [ $location == "left" ];
 then
 
     xdotool windowsize $focused_window $windowX%, $windowY%
     xdotool windowmove $focused_window 1.5%, 5%
-    if [ $windowname == "Konsole" ]
+    if [ $windowname == "Konsole" ] || [ $windowname == "Dolphin" ];
     then
         xdotool windowmove --relative $focused_window 0, 24
     fi
 fi
 
-if [ $location == "right" ]
+if [ $location == "right" ];
 then
 
     xdotool windowsize $focused_window $windowX%, $windowY%
     xdotool windowmove $focused_window $tilingright%, 5%
-
-    if [ $windowname == "Konsole" ]
+ 
+    if [ $windowname == "Konsole" ] || [ $windowname == "Dolphin" ];
     then
         xdotool windowmove --relative $focused_window 0, 24
     fi
