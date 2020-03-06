83506f773da5f53bfe043f2f9a475ad01f483cca
jakobkhansen <jakob.hansen@hotmail.no>
2020-03-06 20:23:26 +0100

    Latex, updates

---
+++ latextemplate.sh
@@ -0,0 +1,31 @@
+filename=$1
+
+touch $filename
+
+echo '\documentclass[12pt]{article}
+
+\usepackage[norsk]{babel}
+\usepackage{graphicx}
+\usepackage{amsmath}
+\usepackage{tikz}
+\usetikzlibrary{automata, positioning, arrows}
+
+\tikzset{
+    ->,
+    node distance=3cm,
+    initial text=$ $,
+}
+
+\title{\vspace*{-3cm}Tittel}
+
+\author{Jakob Hansen}
+
+\date{\today}
+
+\begin{document}
+\maketitle
+
+\section*{Oppgave 1}
+\subsection*{1a}
+
+\end{document}' > $filename
