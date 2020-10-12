c6a24857c2ebad5d9f15345ff3d18e7f07f6a74c
jakobkhansen <jakob.hansen@hotmail.no>
2020-10-12 21:41:13 +0200

    Update scripts

--- CommentaryMovie.sh
+++ CommentaryMovie.sh
@@ -14,7 +14,7 @@ echo "Inserting silence to commentary"
 ffmpeg -i "/tmp/"$commentary"-cut.mp3" -af "adelay="$movieoffset"s" "/tmp/"$commentary"-final.mp3"
 
 echo "Extracting movie audio"
-ffmpeg -i $movie "/tmp/"$movie"-audio.mp3"
+ffmpeg -i $movie -filter:a "volume=1.1" "/tmp/"$movie"-audio.mp3"
 
 echo "Combining commentary and movie audio"
 ffmpeg -i "/tmp/"$movie"-audio.mp3" -i "/tmp/"$commentary"-final.mp3" -filter_complex amerge=inputs=2 -ac 2 "/tmp/"$movie"-audiofinal.mp3" -vsync 2
